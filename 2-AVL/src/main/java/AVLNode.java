0c5ddee60a748b4342d7e4a5e446cee47e9743c3
Rebecca Turner <637275@gmail.com>
2018-03-07 15:13:53 -0500

    doubles and equals for avlnode

--- 2-AVL/src/main/java/AVLNode.java
+++ 2-AVL/src/main/java/AVLNode.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2018 Rebecca Turner (rebeccaturner@brandeis.edu)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.util.Comparator;
 import java.util.Objects;
 
@@ -60,7 +77,11 @@ public class AVLNode<T> {
     }
 
     /**
-     * a lower bound on the tree's height; actual height of bottom leaves may be greater than this by 1
+     * a lower bound on the tree's height; actual height of bottom leaves may be
+     * greater than this by 1
+     *
+     * O(log n) time because there's no way in hell i'm adding another field to
+     * this bloated class
      */
     public int approximateHeight() {
         return 1 + leftChild.approximateHeight();
@@ -134,16 +155,34 @@ public class AVLNode<T> {
     }
 
     private String toStringDotUnwrapped() {
-        return data.toString() + " -> " + leftChild.toString() + ";\n"
-            + data.toString() + " -> " + rightChild.toString() + ";\n";
+        return "\"" + data.toString()
+                + "\" -> \"" + leftChild.toString() + "\";\n"
+                + "\"" + data.toString()
+                + "\" -> \"" + rightChild.toString() + "\";\n";
     }
 
     /**
      * creates a GraphViz dot representation of this tree
      */
     public String toStringDot() {
-        return toStringDotUnwrapped()
+        return "digraph {\nnode[shape=box];"
+                + toStringDotUnwrapped()
                 + leftChild.toStringDotUnwrapped()
-                + rightChild.toStringDotUnwrapped();
+                + rightChild.toStringDotUnwrapped()
+                + "}\n";
+    }
+
+    /**
+     * uses a fairly large epsilon for comparing `data` fields
+     *
+     */
+    @Override
+    public boolean equals(Object o) {
+        if(!(o instanceof AVLNode)) {
+            return false;
+        }
+        AVLNode<?> n = (AVLNode<?>) o;
+        return Objects.equals(data, n.data)
+                && Doubles.equals(value, n.value, 0.000001);
     }
 }
--- 2-AVL/src/test/java/AVLNodeTest.java
+++ 2-AVL/src/test/java/AVLNodeTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2018 Rebecca Turner (rebeccaturner@brandeis.edu)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,6 +41,10 @@ class AVLNodeTest {
 
     @Test
     void hasParent() {
+        AVLNode<Integer> n = new AVLNode<>(100, 9.9);
+        // insert on left
+        n.insert(100, 20.0);
+        assertTrue(n.getRightChild().hasParent());
     }
 
     @Test
@@ -36,6 +57,10 @@ class AVLNodeTest {
 
     @Test
     void getParent() {
+        AVLNode<Integer> n = new AVLNode<>(100, 9.9);
+        // insert on left
+        n.insert(100, 20.0);
+        assertEquals(new AVLNode<>(100, 20.0), n.getRightChild().getParent());
     }
 
     @Test
